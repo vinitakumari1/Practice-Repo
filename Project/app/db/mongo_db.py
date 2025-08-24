0b4bd2cab446985c62cc529c05d27386ec2a7650
Vinita Kumari <vineetha.yelamanchi@thezennialpro.com>
2025-08-24 16:54:28 +0530

    Added index to mongo for faster query times

--- Project/app/db/mongo_db.py
+++ Project/app/db/mongo_db.py
@@ -6,3 +6,9 @@ client = MongoClient(MONGO_URL)
 
 mongo_db = client["weather_db"]
 weather_collection = mongo_db["weather_forecast"]
+
+
+weather_collection.create_index([("city", 1)])
+
+# Compound index on city + time (useful if you query city & date together)
+weather_collection.create_index([("city", 1), ("time", 1)])
